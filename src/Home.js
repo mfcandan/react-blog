0e8052c55cf7187e53176a00310e1c9555e335d1
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-06 17:58:03 +0300

    Handle Erros!

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => { 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok){
+                    throw Error('could not fetch the data that resource');
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setIsPending(false);
-            });
+                setError(null);
+            })
+            .catch((err) => {
+                setIsPending(false);
+                setError(err.message);
+            })
     },[]);
 
     return ( 
         <div className="home">
-            { isPending && <div>Loading...</div> }
-           {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+            {error && <div> {error} </div> }
+            {isPending && <div>Loading...</div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
 }
