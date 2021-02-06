867d1c79e035080950408c123c39db9620d7815d
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-06 15:57:13 +0300

    get dummy blog datas and create blogs with map & blog css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new site', body: 'lorem ipsum dolor', author:'fatih', id:1},
+        {title: '2My new site', body: 'lorem ipsum dolor', author:'haydar', id:2},
+        {title: '3My new site', body: 'lorem ipsum dolor', author:'orhan', id:3}
+    ])
+
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
