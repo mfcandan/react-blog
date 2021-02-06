b6323ecb8e8c391115806630d6660725cf1759fe
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-06 17:13:21 +0300

    useEffect for fecth data at first time

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,22 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
-const Home = () => {
-    const [name, setName] = useState('fatih');
-    
-    const [blogs, setBlogs] = useState([
-        {title: 'My new site', body: 'lorem ipsum dolor', author:'fatih', id:1},
-        {title: '2My new site', body: 'lorem ipsum dolor', author:'haydar', id:2},
-        {title: '3My new site', body: 'lorem ipsum dolor', author:'fatih', id:3}
-    ])
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);              
-    }
+const Home = () => { 
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('Use effect run');
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            });
     },[]);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            <button onClick={()=> setName('Mario')}>Change Name</button>
-            <p>{name}</p>
+           {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
 }
