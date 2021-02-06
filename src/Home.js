73dfccf847c20fb3a61b524ec3d86d540a97e71c
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-06 16:12:06 +0300

    filter() added

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My new site', body: 'lorem ipsum dolor', author:'fatih', id:1},
         {title: '2My new site', body: 'lorem ipsum dolor', author:'haydar', id:2},
-        {title: '3My new site', body: 'lorem ipsum dolor', author:'orhan', id:3}
+        {title: '3My new site', body: 'lorem ipsum dolor', author:'fatih', id:3}
     ])
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'fatih')} title="Fatih's Blogs" />
         </div>
      );
 }
