1a48159181fceed992b37c8ff5248de7cbe0900f
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-06 16:43:06 +0300

    useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+    const [name, setName] = useState('fatih');
+    
     const [blogs, setBlogs] = useState([
         {title: 'My new site', body: 'lorem ipsum dolor', author:'fatih', id:1},
         {title: '2My new site', body: 'lorem ipsum dolor', author:'haydar', id:2},
@@ -13,9 +15,16 @@ const Home = () => {
         setBlogs(newBlogs);              
     }
 
+    useEffect(() => {
+        console.log('Use effect run');
+        console.log(name);
+    },[name]);
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={()=> setName('Mario')}>Change Name</button>
+            <p>{name}</p>
         </div>
      );
 }
