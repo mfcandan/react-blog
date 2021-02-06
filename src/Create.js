5d9afa652f57f443b3c03f50e2feef34a064e037
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-06 22:42:11 +0300

    Form submit & handleSubmit

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,18 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('fatih');
 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author};
+        
+        console.log(blog);
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}> 
                 <label>Blog Title:</label>
                 <input 
                     type="text"
