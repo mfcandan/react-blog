0997d0f925a5036c97553b8a45432f9135fd46f4
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-06 22:34:27 +0300

    Create Form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('fatih');
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    placeholder="My Title"
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                    placeholder="Blog Content"
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Auther:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="fatih">fatih</option>
+                    <option value="haydar">haydar</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
